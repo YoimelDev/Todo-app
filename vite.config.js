8fd8a274d630884193b043c00f8ec9ffde5584fc
Yoimel Rojas <yoimeldev@gmail.com>
2022-04-07 09:35:09 -0400

    add base and build dir

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: 'https://YoimelDev.github.io/Todo-app/',
+  build: {
+    outDir: 'docs'
+  }
 })
